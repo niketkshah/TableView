89f08858eaee12d54ceaf2e0a5c28be17b8fdf2b
niketkshah <shahniket05@gmail.com>
2017-02-08 21:48:10 -0500

    Add files via upload

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1
+    }
+    
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text="Top row"
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
